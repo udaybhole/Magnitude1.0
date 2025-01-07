148f5780d0972720fdeee372bbe2d5d2d0c212e2
Dinesh <143598778+Dinesh-dypcoe@users.noreply.github.com>
2025-01-07 13:06:36 +0530

    scroll on top

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,37 @@ import About from "./pages/About";
 import VantaBackground from "./components/VantaBackground";
 import FAQ from "./pages/FAQ";
 import Footer from './components/Footer';
+import useScrollToTop from './hooks/useScrollToTop';
+
+function AppContent() {
+  useScrollToTop();
+
+  return (
+    <div className="relative min-h-screen w-full overflow-hidden">
+      <VantaBackground />
+      <div className="relative z-10">
+        <Navbar />
+        <main className="w-full max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/problems" element={<ProblemStatements />} />
+            <Route path="/guidelines" element={<Guidelines />} />
+            <Route path="/committees" element={<Committees />} />
+            <Route path="/submit" element={<IdeaSubmission />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/faq" element={<FAQ />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="relative min-h-screen w-full overflow-hidden">
-        <VantaBackground />
-        <div className="relative z-10">
-          <Navbar />
-          <main className="w-full max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/problems" element={<ProblemStatements />} />
-              <Route path="/guidelines" element={<Guidelines />} />
-              <Route path="/committees" element={<Committees />} />
-              <Route path="/submit" element={<IdeaSubmission />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/faq" element={<FAQ />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 }
